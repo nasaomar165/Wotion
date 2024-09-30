198acf87b9c250700972f2bfc80a8308cb806369
Omar Hisham <omar.hisham0597@gmail.com>
2024-09-30 20:25:06 +0300

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import { Toaster } from "sonner";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { ModelProvider } from "@/components/providers/model-provider";
+import { EdgeStoreProvider } from "@/lib/edgestore";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Wotion",
+  description: "The Connecting workspace where better, faster work happens",
+  icons: {
+    icon: "/dark-logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark:bg-[#1f1f1f]" suppressHydrationWarning>
+      <body className={cn(inter.className, "dark:bg-[#1f1f1f]")}>
+        <ConvexClientProvider>
+          <EdgeStoreProvider>
+            <ThemeProvider
+              attribute="class"
+              enableSystem
+              defaultTheme="system"
+              disableTransitionOnChange
+              storageKey="Wotion"
+            >
+              <Toaster position="bottom-right" />
+              <ModelProvider />
+              {children}
+            </ThemeProvider>
+          </EdgeStoreProvider>
+        </ConvexClientProvider>
+      </body>
+    </html>
+  );
+}
